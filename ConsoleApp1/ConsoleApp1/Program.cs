7b22a3145766b91c49fb679fa1d15b21a5e8ac75
Maruf-Robin <maruf.robin131313@gmail.com>
2019-09-10 00:04:29 +0600

    Added for firstTime

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace myNameSpace
+{
+    class FisrtClass
+    {
+        static void Main(string[] args)
+        {
+            string name, address, contact;
+            int age;
+            Console.Write("Enter your name : ");
+            name = Console.ReadLine();
+            Console.Write("Enter your Age : ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your Address : ");
+            address = Console.ReadLine();
+            Console.Write("Enter your Contact : ");
+            contact = Console.ReadLine();
+
+            Console.WriteLine("\n--------o------\nName\t: " + name + "\nAge\t: " + age + "\nAddress\t: " + address + "\nContact : " + contact);
+
+            Console.ReadKey();
+
+        }
+    }
+}
